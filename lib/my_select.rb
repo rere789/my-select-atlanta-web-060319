965f116540b00444e4208091445d383dae30379c
Reina Ewing <reina.castro3@gmail.com>
2019-05-19 03:18:33 +0000

    Done.

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,13 @@
 def my_select(collection)
- # your code here!
+  i = 0 
+  new_array = []
+    while i < collection.size 
+    if yield(collection[i]) == true  
+      new_array.push(collection[i])
+    end
+      i += 1 
+  end 
+  new_array
 end
+
+

375c69ba2e272769b723bc4c18dd82571888fd04
Reina Ewing <reina.castro3@gmail.com>
2019-05-19 03:17:01 +0000

    Automatically backed up by Learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,26 +1,26 @@
 # def my_select(collection)
 # # your code here!
 # i = 0 
-# new_array = [ ]
+# new_array = []
 #   while i < collection.size 
 #     if yield(collection[i]) == true  
 #       result.push(collection[i])
-#       i += 1 
 #     end
-#     new_array
+      i += 1 
 #   end 
+new_array
 # end
 
 
-def my_select(array) 
-count = 0
-result = []
+# def my_select(array) 
+# count = 0
+# result = []
 
-while count < array.size
-  if yield(array[count]) == true
-  result.push(array[count])
-end
-  count+=1
-end
-result
-end
+# while count < array.size
+#   if yield(array[count]) == true
+#   result.push(array[count])
+# end
+#   count+=1
+# end
+# result
+# end

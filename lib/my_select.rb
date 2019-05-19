543fe3a6336c707ea6c458df2e6ff0072cc77dfb
Reina Ewing <reina.castro3@gmail.com>
2019-05-19 03:15:28 +0000

    Automatically backed up by Learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -10,3 +10,17 @@
 #     new_array
 #   end 
 # end
+
+
+def my_select(array) 
+count = 0
+result = []
+
+while count < array.size
+  if yield(array[count]) == true
+  result.push(array[count])
+end
+  count+=1
+end
+result
+end

48a6dcdc3959fe4b4e48eaa780caaa4a26cf6f9d
Reina Ewing <reina.castro3@gmail.com>
2019-05-19 03:17:22 +0000

    Automatically backed up by Learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,11 +1,11 @@
-# def my_select(collection)
-# # your code here!
-# i = 0 
-# new_array = []
-#   while i < collection.size 
-#     if yield(collection[i]) == true  
-#       result.push(collection[i])
-#     end
+def my_select(collection)
+# your code here!
+i = 0 
+new_array = []
+  while i < collection.size 
+    if yield(collection[i]) == true  
+      result.push(collection[i])
+    end
       i += 1 
   end 
 new_array
